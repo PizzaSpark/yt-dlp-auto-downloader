68a30c44c7e54e84b0d8e62b66cacb072a8cbad2
Seanti <seanpizzaspark@gmail.com>
2024-04-16 21:31:29 +0800

    started feature of batch downloading

--- scripts/download.bat
+++ scripts/download.bat
@@ -8,27 +8,37 @@ echo What do you want to download?:
 echo.
 echo 1. Video
 echo 2. Audio
+echo 3. Video Playlist
+echo 4. Audio Playlist
 echo 3. Exit
 echo.
 set /p choice=Choice: 
 if "%choice%"=="1" goto VIDEO
 if "%choice%"=="2" goto AUDIO
-if "%choice%"=="3" goto END
+if "%choice%"=="3" goto BATCH_VIDEO
+if "%choice%"=="4" goto BATCH_AUDIO
+if "%choice%"=="5" goto END
 echo Invalid choice, please try again.
 pause
 goto MENU
 
 :VIDEO
-call :DOWNLOAD "../downloads/video" "-S "res:1080" --remux-video mp4"
+call :DOWNLOAD "../downloads/video" "%%(title)s.%%(ext)s" "-S "res:1080" --remux-video mp4"
 goto REPEAT
 
 :AUDIO
-call :DOWNLOAD "../downloads/audio" "-x --audio-format mp3"
+call :DOWNLOAD "../downloads/audio" "%%(title)s.%%(ext)s" "-x --audio-format mp3"
 goto REPEAT
+
+:BATCH_VIDEO
+call :DOWNLOAD "../downloads/video" "%%(playlist)s/%%(title)s.%%(ext)s" "-S "res:1080" --remux-video mp4"
+
+:BATCH_AUDIO
+call :DOWNLOAD "../downloads/audio" "%%(playlist)s/%%(title)s.%%(ext)s" "-x --audio-format mp3"
 
 :DOWNLOAD
 set /p input=Input link: 
-yt-dlp -P ".\%~1" -o "%%(title)s.%%(ext)s" %~2 %input% || echo Download failed!
+yt-dlp -P ".\%~1" -o %~2 %~3 %input% || echo Download failed!
 goto :eof
 
 :REPEAT
