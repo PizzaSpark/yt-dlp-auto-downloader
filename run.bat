b39f1994faed3b11cba6705dc4170a7583b537ac
Seanti <seanpizzaspark@gmail.com>
2024-04-16 20:40:10 +0800

    fixed file

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
 
-PowerShell -ExecutionPolicy Bypass -File .\scripts\dl_ytdlp.ps1
+PowerShell -ExecutionPolicy Bypass -File .\scripts\update_code.ps1
 PowerShell -ExecutionPolicy Bypass -File .\scripts\dl_ytdlp.ps1
 PowerShell -ExecutionPolicy Bypass -File .\scripts\dl_ffmpeg.ps1
 
