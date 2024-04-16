8e1b41e2e70c70f7822eca2368c33ae22332d36f
Seanti <seanpizzaspark@gmail.com>
2024-04-16 19:47:26 +0800

    renamed files

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 @echo off
 
-PowerShell -ExecutionPolicy Bypass -File .\scripts\dlYtdlp.ps1
-PowerShell -ExecutionPolicy Bypass -File .\scripts\dlFfmpeg.ps1
+PowerShell -ExecutionPolicy Bypass -File .\scripts\dl_ytdlp.ps1
+PowerShell -ExecutionPolicy Bypass -File .\scripts\dl_ffmpeg.ps1
 
 call .\scripts\download.bat
 
